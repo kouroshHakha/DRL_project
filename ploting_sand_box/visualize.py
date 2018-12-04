cb473d1f17a3977a35a10cc3b5644533d4ad4e5e
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2018-12-04 12:39:44 -0800

    viz.py added with normalization capabilities

--- ploting_sand_box/visualize.py
+++ ploting_sand_box/visualize.py
@@ -3,14 +3,21 @@ import glob
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser('Rinokeras RL Example Script')
+parser.add_argument('--normalize', action='store_true')
+args = parser.parse_args()
 
 files = glob.glob('*.npy')
 
 for f in files:
     data = np.load(f)
+    if args.normalize:
+        data = (data - np.min(data))/ (np.max(data)-np.min(data))
     plt.plot(data)
 plt.xlabel('Iteration')
 plt.ylabel('Avg Episode Reward')
 plt.legend([f.split('.')[0] for f in files])
-plt.savefig('pointmass_results.png')
+plt.savefig('pointmass_mobj_effect.png')
 plt.show()

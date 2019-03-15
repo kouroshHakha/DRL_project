ca804c192e54f0ee8465a0a5cc09d4aaae35160e
ameer <ameerh@berkeley.edu>
2019-03-15 16:23:45 -0700

    validation training

--- val_autobag_ray.py
+++ val_autobag_ray.py
@@ -3,17 +3,14 @@ import ray.tune as tune
 from ray.rllib.agents import ppo
 from ray.rllib.contrib.random_agent.random_agent import RandomAgent
 from envs.discrete_opamp import TwoStageAmp
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--checkpoint_dir', '-cpd', type=str)
+args = parser.parse_args()
 
 ray.init()
-tune.run_experiments({
-    "my_experiment": {
-        "checkpoint_freq":2,
-        "restore": "/home/ksettaluri6/ray_results/my_experiment/PPO_TwoStageAmp_0_2019-03-13_11-13-51i3_j_z4h/checkpoint_438/checkpoint-438",
-        "run": "PPO",
-        "env": TwoStageAmp,
-        "stop": {"episode_reward_max": -0.02},
-        "config": {
-	    #"observation_filter": "NoFilter",
+
+config_validation = {
             "sample_batch_size": 200,
             "train_batch_size": 1200,
             "sgd_minibatch_size": 1200,
@@ -25,6 +22,41 @@ tune.run_experiments({
             "model":{"fcnet_hiddens": [64, 64]},
             "num_workers": 1,
             "env_config":{"generalize":False, "save_specs":False},
-            },
-    },
-})
+            }
+
+config_train = {
+            "sample_batch_size": 200,
+            "train_batch_size": 1200,
+            "sgd_minibatch_size": 1200,
+            "num_sgd_iter":3,
+            "lr":1e-3,
+            "vf_loss_coeff":0.5,
+            "horizon":  60,
+            "num_gpus": 0,
+            "model":{"fcnet_hiddens": [64, 64]},
+            "num_workers": 1,
+            "env_config":{"generalize":True, "save_specs":True},
+            }
+
+if not args.checkpoint_dir:
+    trials = tune.run_experiments({
+        "train_ppo": {
+        "checkpoint_freq":2,
+        "run": "PPO",
+        "env": TwoStageAmp,
+#        "stop": {"training_iteration": 3, "episode_reward_max": -0.02},
+        "stop": {"episode_reward_max": -0.02},
+        "config": config_train},
+    })
+else:
+    print("RESTORING NOW!!!!!!")
+    #print(trials[0]._checkpoint.value)
+    tune.run_experiments({
+        "restore_ppo": {
+        "run": "PPO",
+        "config": config_validation,
+        "env": TwoStageAmp,
+        #"restore": trials[0]._checkpoint.value},
+        "restore": args.checkpoint_dir,
+        "checkpoint_freq":2},
+    })
